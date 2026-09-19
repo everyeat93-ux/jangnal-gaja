f1d9fac2234d3d2c0c2512162efe330175ad2cc2
Brasshun <brasshun@gmail.com>
2026-09-19 23:22:31 +0900

    chore: bump version to 1.3.2 (versionCode 16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.jangnal.gaja"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.3.1"
+        versionCode = 16
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
